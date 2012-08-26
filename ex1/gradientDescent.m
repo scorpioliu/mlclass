581928756d4f7648b654f2b65bf7352f395f45e6
projectsanji <greatprojectsanji1234567890@gmail.com>
2012-08-26 23:28:23 +0800

    ex1 complete

--- ex1/gradientDescent.m
+++ ex1/gradientDescent.m
@@ -16,14 +16,8 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-    size(X*theta -y) 
-    size(X)
 
     theta = theta - alpha * X' * (X*theta -y) / m;
-
-
-
-
 
     % ============================================================
 
